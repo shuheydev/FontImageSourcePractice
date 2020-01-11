9f9a72d98599549302fc7e6435d99c47d70936e5
shuheydev <shuhey.dev@gmail.com>
2020-01-11 22:17:08 +0900

    Add project files.

---
+++ FontImageSourcePractice/FontImageSourcePractice/IconFonts.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FontImageSourcePractice
+{
+	static class IconFont
+	{
+		public const string Video = "\ue800";
+		public const string CancelCircled = "\ue801";
+		public const string Users = "\ue802";
+		public const string Glass = "\ue803";
+		public const string Music = "\ue804";
+		public const string Search = "\ue805";
+		public const string Mail = "\ue806";
+		public const string Heart = "\ue807";
+		public const string HeartEmpty = "\ue808";
+		public const string Star = "\ue809";
+		public const string StarEmpty = "\ue80a";
+		public const string StarHalf = "\ue80b";
+		public const string CancelCircled2 = "\ue80c";
+		public const string User = "\ue80d";
+		public const string Videocam = "\ue811";
+		public const string Picture = "\ue812";
+		public const string CameraAlt = "\ue814";
+		public const string ThLarge = "\ue815";
+		public const string Th = "\ue816";
+		public const string ThList = "\ue817";
+		public const string Ok = "\ue818";
+		public const string OkCircled = "\ue819";
+		public const string OkCircled2 = "\ue81a";
+		public const string Cancel = "\ue81b";
+		public const string PlusCircled = "\ue81d";
+		public const string Minus = "\ue81e";
+		public const string MinusCircled = "\ue81f";
+		public const string HelpCircled = "\ue820";
+		public const string InfoCircled = "\ue821";
+		public const string Link = "\ue823";
+		public const string Attach = "\ue824";
+		public const string Lock = "\ue825";
+		public const string LockOpen = "\ue826";
+		public const string Pin = "\ue827";
+		public const string Eye = "\ue828";
+		public const string EyeOff = "\ue829";
+		public const string LinkExt = "\uf08e";
+		public const string MailAlt = "\uf0e0";
+		public const string PlusSquared = "\uf0fe";
+		public const string Unlink = "\uf127";
+		public const string Help = "\uf128";
+		public const string Info = "\uf129";
+		public const string LockOpenAlt = "\uf13e";
+		public const string MinusSquared = "\uf146";
+		public const string MinusSquaredAlt = "\uf147";
+		public const string OkSquared = "\uf14a";
+		public const string LinkExtAlt = "\uf14c";
+		public const string Female = "\uf182";
+		public const string Male = "\uf183";
+		public const string PlusSquaredAlt = "\uf196";
+		public const string MailSquared = "\uf199";
+		public const string Child = "\uf1ae";
+		public const string UserSecret = "\uf21b";
+		public const string UserPlus = "\uf234";
+		public const string UserTimes = "\uf235";
+	}
+}
